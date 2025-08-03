31e1b0a96d399a0863f33269b3852298fcc208b7
croisen <andrewjames.git.gan@gmail.com>
2025-08-03 17:03:55 +0800

    Somehow errors out when compiled in debian before this change

--- config.mk
+++ config.mk
@@ -13,10 +13,10 @@ X11LIB = /usr/X11R6/lib
 # flags
 CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE -DALSA -DVERSION=\"${VERSION}\"
 CFLAGS   = -std=c99 -pedantic -Wall -Wextra -Wno-unused-parameter -Os
-LDFLAGS  = -L$(X11LIB) -lasound -s
+LDFLAGS  = -L$(X11LIB) -s
 # OpenBSD: add -lsndio
 # FreeBSD: add -lkvm -lsndio
-LDLIBS   = -lX11
+LDLIBS   = -lX11 -lasound
 
 # compiler and linker
 CC = cc
